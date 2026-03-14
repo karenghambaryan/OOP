11c0edc6e7d8b9538039381a3d24a1633a4f9088
root <root@DESKTOP-GNNSMQ4.localdomain>
2026-03-14 04:30:41 +0300

    Console

---
+++ Console/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "console.h"
+
+int main()
+{
+    Tv_Console console_1;
+    console_1.CallFuncs();
+    std::cout <<"\n";
+    Ventilator_Console console_2;
+    console_2.CallFuncs();
+    
+    
+    
+    return 0;
+}
