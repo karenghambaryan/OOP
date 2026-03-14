7d1020edd788554244f1bdba9ee3f92b1b984ab0
root <root@DESKTOP-GNNSMQ4.localdomain>
2026-03-14 04:34:52 +0300

    dynamic_cast

---
+++ dynamic_cast.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+class Animal
+{
+    public:
+        Animal() = default;
+        virtual ~Animal() = default;
+        virtual void Sound() const = 0;
+};
+
+class Dog : public Animal
+{
+    public:
+        Dog() = default;
+        ~Dog() override = default;
+        void Sound() const override
+        {
+            std::cout << "Haf" << "\n";
+        }
+        
+};
+
+class Car 
+{
+    public:
+        Car() = default;
+        virtual ~Car() = default;
+};
+
+bool IsAnimal(Animal* x)
+{
+    if(dynamic_cast<Animal*>(x) != nullptr)
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    Car* c = new Car();
+    Animal* d = new Dog();
+    
+    std::cout << IsAnimal(d) << "\n"; 
+
+
+    return 0;
+}
