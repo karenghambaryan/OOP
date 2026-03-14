67d59940b8881a9588ef4ffadca46b0798bcb59c
root <root@DESKTOP-GNNSMQ4.localdomain>
2026-03-14 04:18:24 +0300

    Zoo

---
+++ Zoo/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Animals.h"
+#include "Zoo.h"
+
+int main()
+{
+    Zoo zoo(5);
+
+    zoo.AddAnimal(new Dog("carnivore", "pet", 4, 5, "Bulldog", "Brown"));
+    zoo.AddAnimal(new Tiger("carnivore", "wild", 4, 7, "Africa"));
+    zoo.AddAnimal(new Monkey("herbivore", "wild", 2, 3, "Capuchin"));
+    zoo.AddAnimal(new Donkey("herbivore", "pet", 4, 6, 120));
+    zoo.AddAnimal(new Horse("herbivore", "pet", 4, 4, 75));
+    std::cout << std::endl;
+    zoo.Sounds();
+    std::cout << std::endl;
+
+    return 0;
+}
